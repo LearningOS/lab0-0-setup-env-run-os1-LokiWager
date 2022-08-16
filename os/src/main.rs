b4fa8ce5b40bcdd9171cdfd256e47082784b174f
Loki's Wager <32408858+LokiWager@users.noreply.github.com>
2022-08-16 01:58:30 +0000

    [homework] os1 -- adding log

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,19 +2,42 @@
 #![no_main]
 #![feature(panic_info_message)]
 
+use log::*;
 
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
+mod logging;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
     clear_bss();
+    logging::init();
     println!("Hello, world!");
+    trace!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    warn!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    error!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     panic!("Shutdown machine!");
 }
 
